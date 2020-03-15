d2c42099d8a1fba3051400b92c09330516bed31d
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2020-03-15 10:51:23 +0200

    clarify ShowOrderBookCmd output (columns);

--- src/main/scala-2.12/org/ergoplatform/appkit/ergotool/dex/ShowOrderBookCmd.scala
+++ src/main/scala-2.12/org/ergoplatform/appkit/ergotool/dex/ShowOrderBookCmd.scala
@@ -33,11 +33,11 @@ case class ShowOrderBookCmd(toolConf: ErgoToolConfig,
 
       console.println(s"Order book for token $tokenId:")
       console.println("Sell orders:")
-      console.println("Amount   Total")
+      console.println("Amount   Total(including DEX fee)")
       sellOrders.foreach { o => console.println(f"${o.tokenAmount}%8d ${o.tokenPriceWithDexFee}") }
 
       console.println("Buy orders:")
-      console.println("Amount   Total")
+      console.println("Amount   Total(including DEX fee)")
       buyOrders.foreach { o => console.println(f"${o.tokenAmount}%8d ${o.tokenPriceWithDexFee}") }
     })
   }
